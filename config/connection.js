5fe991e063498d323fcfac748ff0314c5c4fce26
LauraAy <laura.ay1564@gmail.com>
2020-03-02 23:04:48 -0800

    adding JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 const { createConnection } = require('mysql2')
+var connection;
 
-const connection = createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+connection = createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: '',
   database: 'burger_db'
-})
+});
+};
 
+connection.connect();
 module.exports = connection
